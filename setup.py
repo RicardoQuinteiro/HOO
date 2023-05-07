33b4d2fd460515b7b9caa63a0ccee082e60e11cf
Ricardo Quinteiro <ricardo.lopes.santos@fidelidade.pt>
2023-05-07 21:13:16 +0000

    fix: python version and added pydantic

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="hoo",
-    version="0.1.6",
+    version="0.1.0",
     description="Algorithms that derive from Hierarchical Optimistic Optimization (HOO)",
     long_description_content_type="text/markdown",
     setup_requires=["setuptools>=38.6.0"],
     packages=find_packages(),
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     install_requires=[
         "gym==0.26.2",
-        "matplotlib==3.7.1"
+        "matplotlib==3.7.1",
+        "pydantic==1.10.4",
     ],
 )
